40c82bd71e0cf81c7ea53157d2bd6dd3edea9d42
TheGVictor <gabrielsancinetti@gmail.com>
2024-02-26 09:52:11 -0300

    Starting to implement board and pieces

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Board board = new Board(8, 8);
+		
+		board.equals(board);
+	}
+
+}
